ab2a2148b3a5da9a95391e4ab0d179532870cd38
Jordan Kline <jordan.kline@gmail.com>
2018-01-04 20:11:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,18 +2,19 @@ def badge_maker(name)
   p "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
+def batch_badge_creator(array)
   return_array = []
-    attendees.each { |el| return_array << badge_maker(el) }
+    array.each { |el| return_array << badge_maker(el) }
   return_array
 end
 
 def assign_rooms(array)
   return_array = []
-  array.each_with_index {|el, i| return_array << "Hello, #{el}! You'll be assigned to room #{i + 1}!" }
+    array.each_with_index {|el, i| return_array << "Hello, #{el}! You'll be assigned to room #{i + 1}!" }
   return_array
 end
 
-def printer
-
+def printer(array)
+  puts batch_badge_creator(array)
+  puts assign_rooms(array)
 end
