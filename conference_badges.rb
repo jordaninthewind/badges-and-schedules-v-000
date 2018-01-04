89d023c0b13ae4055b7dde4cf62e6069d4e677a1
Jordan Kline <jordan.kline@gmail.com>
2018-01-04 19:51:08 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker(array)
+  array.each {|el| p "Hello, my name is #{name}."}
+end
+
+def batch_badge_creator
+
+end
+
+def assign_rooms
+
+end
+
+def printer
+
+end
