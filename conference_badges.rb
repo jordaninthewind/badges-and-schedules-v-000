0ebeedc4d3a4322ebb2dc2bc0ec4534415ca902d
Jordan Kline <jordan.kline@gmail.com>
2018-01-04 20:06:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,8 +8,9 @@ def batch_badge_creator(attendees)
   return_array
 end
 
-def assign_rooms
-
+def assign_rooms(array)
+  counter = 1
+  array.each_with_index {|el, i| p "Hello, #{el}! You'll be assigned to room #{i + 1}!" }
 end
 
 def printer
